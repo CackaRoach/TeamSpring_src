4fecbc3622fadbb082a7af6ea5e5f5f699202270
yoojae <dhakdlrkt3@naver.com>
2019-10-28 17:53:47 +0900

    출력하는거 만드는중

--- KAIKEI/src/main/java/com/ts/kaikei/dao/StatementDAO.java
+++ KAIKEI/src/main/java/com/ts/kaikei/dao/StatementDAO.java
@@ -1,8 +1,19 @@
 package com.ts.kaikei.dao;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ts.kaikei.vo.StatementVO;
+
 
 @Mapper
 public interface StatementDAO {
-
+	public void addStatement(StatementVO boardVO);
+	public void deleteStatement(int idx);
+	public ArrayList<StatementVO> getStatementList();
+	public ArrayList<StatementVO> getStatementListCond(Map<String, String> searchParams);
+	public StatementVO getStatementDetail(int idx);
+	public void updateStatement(StatementVO boardVO);
 }
--- KAIKEI/src/main/java/com/ts/kaikei/services/impl/ReportServiceImpl.java
+++ KAIKEI/src/main/java/com/ts/kaikei/services/impl/ReportServiceImpl.java
@@ -1,10 +1,52 @@
 package com.ts.kaikei.services.impl;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import com.ts.kaikei.services.ReportService;
+import com.ts.kaikei.vo.StatementVO;
 
 @Service("reportService")
 public class ReportServiceImpl implements ReportService {
 
+	@Override
+	public void addStatement(StatementVO boardVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteStatement(int idx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<StatementVO> getStatementList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<StatementVO> getStatementListCond(Map<String, String> searchParams) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatementVO getStatementDetail(int idx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateStatement(StatementVO boardVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
