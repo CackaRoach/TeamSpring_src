82cadb4fffc93e4e84a5ad74daac8961a91319b2
lshan96 <his5423@gmail.com>
2019-10-30 22:06:30 +0900

    10시 전 커밋10-30 22:06

--- KAIKEI/src/main/java/com/ts/kaikei/controllers/ManageController.java
+++ KAIKEI/src/main/java/com/ts/kaikei/controllers/ManageController.java
@@ -1,5 +1,9 @@
 package com.ts.kaikei.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,19 +27,45 @@ public class ManageController {
 	 *  @RequestMapping(value = "/manage/*.do")
 	 */
 	
-	//1. COMPANY MANAGEMENT
+	//1. COMPANY MANAGEMENT - LIST
 	@RequestMapping(value = "/manage/company.do", method = RequestMethod.GET)
-	public String ledger( Model model ) {
+	public String ledger( 	Model model,
+							String PAGE,
+							String FIND) {
 		logger.info("Call : /manage/company.do - GET");
+		
+		if(PAGE == "" || PAGE == null) {
+			PAGE = "1";
+		}
+		
+		
+		
+		
 		
 		return "/manage/company";
 	} 
 	
-	//2. USER MANAGEMENT
+	//2. USER MANAGEMENT - LIST
 	@RequestMapping(value = "/manage/users.do", method = RequestMethod.GET)
-	public String userList( Model model ) {
+	public String userList( Model model,
+							String PAGE,
+							String FIND) {
 		
-		return "/manage/user";
+		Map<String, Object> Params = new HashMap<String, Object>();
+		
+		if(PAGE == "" || PAGE == null) {
+			PAGE = "1";
+		}
+		
+		Params.put("PAGE", PAGE);
+		Params.put("FIND", FIND);
+		
+		/*
+		 * List<UserVo> UserList = manageService.getUser(Params);
+		 * 
+		 */
+		 return "/manage/user";
+		
 	} 
 	
 	//3. CODE MANAGEMENT
--- KAIKEI/src/main/java/com/ts/kaikei/services/ManageService.java
+++ KAIKEI/src/main/java/com/ts/kaikei/services/ManageService.java
@@ -1,5 +1,11 @@
 package com.ts.kaikei.services;
 
+import java.util.List;
+import java.util.Map;
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User;
+
 public interface ManageService {
 
+	public List<User> getUser(Map<String, Object> Params);
 }
